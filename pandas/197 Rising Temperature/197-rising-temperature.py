30cc329789906b9972c48aaabe8f79af064a5e63
krisrvis <krubianodesign@gmail.com>
2025-01-02 17:10:47 -0500

    Revision.

--- pandas/197 Rising Temperature/197-rising-temperature.py
+++ pandas/197 Rising Temperature/197-rising-temperature.py
@@ -2,7 +2,5 @@ import pandas as pd
 import datetime as dt
 
 def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
-    w = weather.sort_values(by="recordDate", ascending=True)
-    w["tempdiff"] = w["temperature"].diff()
-    w["datediff"] = w["recordDate"].diff().dt.days
-    return w[(w["tempdiff"] > 0) & (w["datediff"] == 1)]
+    weather.sort_values(by="recordDate", inplace=True)
+    return weather[(weather.temperature.diff() > 0) & (weather.recordDate.diff().dt.days == 1)][["id"]]
