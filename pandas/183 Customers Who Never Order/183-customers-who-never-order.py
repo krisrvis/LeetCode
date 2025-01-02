b8f3e2dfe8d954e50c8288747fde838adddc9800
krisrvis <krubianodesign@gmail.com>
2025-01-02 17:07:56 -0500

    Revision.

--- pandas/183 Customers Who Never Order/183-customers-who-never-order.py
+++ pandas/183 Customers Who Never Order/183-customers-who-never-order.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
-    df = customers.merge(orders, left_on="id", right_on="customerId", how="left")
-    noorders = df[df["id_y"].isna()]
-    df = noorders.rename(columns={"name":"Customers"})
+    df = customers[~customers["id"].isin(orders["customerId"])]
+    df = df[["name"]].rename(columns={"name": "Customers"})
     return df
